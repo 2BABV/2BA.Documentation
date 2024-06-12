eb9a323ad74de3143db019590da8c8873f95e535
QuincyTromp <quincy@ttt.nl>
2024-06-12 17:04:12 +0200

    Setup sphinx

---
+++ source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '2BA.SphinxDemo'
+copyright = '2024, TTT'
+author = 'Quincy Tromp'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_rtd_theme']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
